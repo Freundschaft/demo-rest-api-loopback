d454451d49616fdf4317a02a3b6c867ce2cfbefd
Qiong Wu <qiong.wu@gfnork.de>
2017-05-02 03:39:54 +0800

    only verify articleid if it is set (todo, disallow unlinking of comments from articleid)

--- common/models/comment.js
+++ common/models/comment.js
@@ -7,28 +7,24 @@ module.exports = function (Comment) {
       var s = ctx.instance;
       var articleId = s.__data.articleId;
 
-      //Get the Application object which the model attached to, and we do what ever we want
-      Comment.getApp(function (err, app) {
-        //App object returned in the callback
-        //PersistedModel.exists(id, callback ((err, exists)))
-        app.models.article.exists(articleId, function (err, exists) {
-          if (err) throw err;
-          if (!exists) {
-            return next(new Error('Bad foreign key...'));
-          } else {
-            return next();
-          }
+      if(articleId) {
+
+        //Get the Application object which the model attached to, and we do what ever we want
+        Comment.getApp(function (err, app) {
+          //App object returned in the callback
+          //PersistedModel.exists(id, callback ((err, exists)))
+          app.models.article.exists(articleId, function (err, exists) {
+            if (err) throw err;
+            if (!exists) {
+              return next(new Error('Bad foreign key...'));
+            } else {
+              return next();
+            }
+          });
         });
-      });
+      } else {
+        next();
+      }
     }
-  });
-
-  Comment.reply = function (msg, cb) {
-    cb(null, 'Greetings... ' + msg);
-  };
-
-  Comment.remoteMethod('reply', {
-    accepts: {arg: 'msg', type: 'comment'},
-    returns: {arg: 'comment', type: 'comment'}
   });
 };
